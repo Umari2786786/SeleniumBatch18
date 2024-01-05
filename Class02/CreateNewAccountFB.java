f3f5c0be55d52e09a92b7ae58412ce15a448039f
umari2786786 <oiqbal2786@gmail.com>
2024-01-05 04:10:42 +0000

    commit 04/01/2024 2nd commit

--- Class02/CreateNewAccountFB.java
+++ Class02/CreateNewAccountFB.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CreateNewAccountFB {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
         // opens chrome browser
         WebDriver driver=new ChromeDriver();
 
@@ -35,10 +36,14 @@ public class CreateNewAccountFB {
         password.sendKeys("abcd");
 
         // locates the create new account button
-        //this is one way another way below!! -- WebElement createAccount = driver.findElement(By.linkText("Create new account"));
+        //this is one way another way below!! --
+        //WebElement createAccount = driver.findElement(By.linkText("Create new account"));
         WebElement createAccount=driver.findElement(By.partialLinkText("new account"));
         // click the create account button
         createAccount.click();
+        Thread.sleep(50000);
+
+        driver.quit();
 
 
 
